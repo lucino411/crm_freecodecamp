a143a646b705c61b9e1e2b243e7ab9a7792dfdb0
lucino411 <lucino411@gmail.com>
2023-10-19 19:07:33 -0500

    mostrar y borrar registros

--- dcrm/website/urls.py
+++ dcrm/website/urls.py
@@ -5,4 +5,27 @@ urlpatterns = [
     path('', views.home, name = 'home' ),
     # path('login/', views.login_user, name = 'login' ),
     path('logout', views.logout_user, name = 'logout' ),
+    path('record/<int:pk>', views.customer_record, name = 'record' ),
+    path('delete_record/<int:pk>', views.delete_record, name = 'delete_record' ),
 ]
+
+'''
+El parámetro name en la función path en las URLs de Django se utiliza para identificar de manera única una URL particular en el proyecto. Proporcionar un nombre a una URL puede ser útil en varias situaciones:
+
+Referencia en plantillas: Puedes usar el nombre proporcionado para referenciar la URL desde las plantillas de Django. Esto es particularmente útil para construir enlaces de forma dinámica en las plantillas.
+
+Reverse URL matching: Puedes utilizar el nombre de la URL para realizar coincidencias inversas (reverse URL matching) en tu código de Django. Esto te permite evitar codificar manualmente las URL en tu aplicación y en su lugar utilizar los nombres de las URLs definidas en las configuraciones de las vistas.
+
+Por ejemplo, si tienes un enlace en tu plantilla que apunta a la vista 'home', podrías usar algo como:
+
+<a href="{% url 'home' %}">Home</a>
+Esto generaría un enlace que apunta a la URL correspondiente a la vista con el nombre 'home' definida en tus archivos de URL.
+
+Además, en tu código de Django, puedes usar el nombre de la URL para realizar coincidencias inversas, lo que te permite generar dinámicamente la URL asociada a un nombre particular:
+
+from django.urls import reverse
+
+url = reverse('home')
+El parámetro name proporciona una forma de referenciar las URLs de manera legible y lógica en tu aplicación de Django, lo que facilita la gestión y el mantenimiento del código a lo largo del tiempo.
+
+'''
--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import Record
 
 # cada vez que vas a una pagina web estas pidiendo (request) algo. Esa peticion (request) se devuelve de nuevo al backend, la pasamos en una vista y devuelve algo, en la primera vista estamos devolviendo el home.html
 
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,10 +22,30 @@ def home(request):
             messages.success(request, 'There was and Error Loggin In, Plesae try again')
             return redirect('home')
     else: 
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
     
 
 def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out')
     return redirect('home')
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        # Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else: 
+        messages.success(request, "You must be logged In to view that page")
+        return redirect('home')
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete_it = Record.objects.get(id=pk)
+        delete_it.delete()
+        messages.success(request, "Record have been deleted successfully")
+        return redirect('home')
+    else:
+        messages.success(request, "You must be logged In to do that")
+        return redirect('home')
